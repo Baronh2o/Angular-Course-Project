0a48fc7b8896e4d96a93502b35be941089a3c16e
Dustin Lunsford <lunsford.dustin@gmail.com>
2018-04-04 09:41:26 -0400

    another round of course project updates

--- angular-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,12 +7,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe [] = [
     new Recipe('A Test Recipe', 'This is simply a test',
     'https://get.pxhere.com/photo/dish-food-recipe-snack-fast-food-cuisine-sandwich' +
     '-vegetarian-food-baked-goods-power-supply-junk-food-beirut-mediterranean-food-' +
     'finger-food-european-food-breakfast-sandwich-american-food-1375814.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test',
+    new Recipe('Another Test Recipe', 'This is simply a test',
     'https://get.pxhere.com/photo/dish-food-recipe-snack-fast-food-cuisine-sandwich' +
     '-vegetarian-food-baked-goods-power-supply-junk-food-beirut-mediterranean-food-' +
     'finger-food-european-food-breakfast-sandwich-american-food-1375814.jpg')
@@ -21,6 +22,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
