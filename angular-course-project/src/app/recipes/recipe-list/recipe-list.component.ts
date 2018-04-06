a33c690bdc307e9620698ff4e9368f5bff5152ea
Dustin Lunsford <lunsford.dustin@gmail.com>
2018-04-06 10:53:58 -0400

    section 10 course project updates (using services)

--- angular-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,25 +8,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe [] = [
-    new Recipe('A Test Recipe', 'This is simply a test',
-    'https://get.pxhere.com/photo/dish-food-recipe-snack-fast-food-cuisine-sandwich' +
-    '-vegetarian-food-baked-goods-power-supply-junk-food-beirut-mediterranean-food-' +
-    'finger-food-european-food-breakfast-sandwich-american-food-1375814.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test',
-    'https://get.pxhere.com/photo/dish-food-recipe-snack-fast-food-cuisine-sandwich' +
-    '-vegetarian-food-baked-goods-power-supply-junk-food-beirut-mediterranean-food-' +
-    'finger-food-european-food-breakfast-sandwich-american-food-1375814.jpg')
-  ];
+  recipes: Recipe [];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
-
 }
