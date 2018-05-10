6866e720881ca92f4e5a37a8da4709624815cf78
Dustin Lunsford <lunsford.dustin@gmail.com>
2018-05-10 15:24:30 -0400

    section 24

--- angular-course-project/src/app/core/header/header.component.ts
+++ angular-course-project/src/app/core/header/header.component.ts
@@ -1,30 +1,36 @@
-import { AuthService } from '../../auth/auth.service';
-import { DataStorageService } from '../../shared/data-storage.service';
-import { Component } from "@angular/core";
+import { Observable } from 'rxjs/observable';
+import { Component, OnInit } from "@angular/core";
+import { Store } from '@ngrx/store';
+
+import * as fromApp from '../../store/app.reducers';
+import * as fromAuth from '../../auth/store/auth.reducers';
+import * as AuthActions from '../../auth/store/auth.actions';
+import * as RecipeActions from '../../recipes/store/recipe.actions';
 //import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService,
-              public authService: AuthService) {}
+export class HeaderComponent implements OnInit {
+  authState: Observable<fromAuth.State>;
+
+  constructor(private store: Store<fromApp.AppState>) {
+  }
+
+  ngOnInit() {
+    this.authState =  this.store.select('auth');
+  }
 
   onSaveData() {
-    this.dataStorageService.storeRecipes()
-      .subscribe(
-        (response) => {
-          console.log(response);
-        }
-      );
+    this.store.dispatch(new RecipeActions.StoreRecipes());
   }
 
   onFetchData() {
-    this.dataStorageService.getRecipes();
+    this.store.dispatch(new RecipeActions.FetchRecipes());
   }
 
   onLogout() {
-    this.authService.logout();
+    this.store.dispatch(new AuthActions.Logout());
   }
 }
--- angular-course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ angular-course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,8 +1,12 @@
+import { Observable } from 'rxjs/observable';
+import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import 'rxjs/add/operator/take';
 
-import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipe.service';
+import * as ShoppingListActions from '../../shopping-list/store/shopping-list.actions';
+import * as fromRecipe from '../store/recipe.reducers';
+import * as RecipeActions from '../store/recipe.actions';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,24 +14,29 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  recipe: Recipe;
+  recipeState: Observable<fromRecipe.State>;
   id: number;
 
-  constructor(private recipeService: RecipeService,
-              private route: ActivatedRoute,
-              private router: Router) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private store: Store<fromRecipe.FeatureState>) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
-        this.recipe = this.recipeService.getRecipe(this.id);
+        this.recipeState = this.store.select('recipes');
       }
     );
   }
 
   onAddToShoppingList () {
-    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+    this.store.select('recipes')
+      .take(1)
+      .subscribe((recipeState: fromRecipe.State) => {
+        this.store.dispatch(new ShoppingListActions.AddIngredients(recipeState.recipes[this.id].ingredients));
+      });
+
   }
 
   onEditRecipe() {
@@ -35,7 +44,7 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   onDeleteRecipe() {
-    this.recipeService.deleteRecipe(this.id);
+    this.store.dispatch(new RecipeActions.DeleteRecipe(this.id));
     this.router.navigate(['/recipes']);
   }
 
